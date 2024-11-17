13fb92b11f685148f88bf5f8838b20f73147e013
majid-felegari <majidfelegari007@gmail.com>
2024-11-17 14:38:04 +0330

    alaki

--- resources/views/surveys/preview.blade.php
+++ resources/views/surveys/preview.blade.php
@@ -1,43 +1,48 @@
 <!DOCTYPE html>
 <html lang="fa">
+
 <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
-    <link rel="stylesheet" href="/panel/bootstrap-4.6.2-dist/css/bootstrap.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <link rel="stylesheet" href="/panel/bootstrap-4.6.2-dist/css/bootstrap.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+        integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="/panel/css/survey.css" rel="stylesheet" />
     <title>کارنامه های برتر</title>
 </head>
 
+
 <!-- main-content -->
+
 <body class="flex">
-<div class="wrapper my-2">
-    <div class="quiz-container">
-        <div class="quiz-head">
-            <h1 class="quiz-title">عنوان پرسش نامه</h1>
-        </div>
-        <div class="quiz-body">
-            <h2 class="quiz-question" id="question">... تو اگه با من قهری</h2>
-            <ul class="quiz-options text-right">
-                <li>من که آشتیم</li>
-                <li>من که دوستتم</li>
-                <li>بیا قهر نباشیم</li>
-                <li>به درک که قهری</li>
-            </ul>
-            <div id="result"></div>
-        </div>
-        <div class="quiz-foot">
-            <button type="button" id="check-answer">ثبت پاسخ</button>
-            <button type="button" id="play-again">Play Again!</button>
-            <div class="quiz-score flex">
-                <span id="correct-score">1</span>/<span id="total-question">10</span>
+    <div class="wrapper my-2">
+        <div class="quiz-container">
+            <div class="quiz-head">
+                <h1 class="quiz-title">عنوان پرسش نامه</h1>
+            </div>
+            <div class="quiz-body">
+                <h2 class="quiz-question" id="question">{{ $survey->question- }}</h2>
+                <ul class="quiz-options text-right">
+                    @foreach($quiz->options as $option) <li>{{ $option }}</li> @endforeach
+                </ul>
+                <div id="result"></div>
+            </div>
+            <div class="quiz-foot">
+            <button type="button" id="next-question">سوال بعدی</button> 
+            <div class="quiz-score flex"> 
+                <span id="current-quiz">1</span>/<span id="total-quiz">10</span> 
+                <div id="question-counter">سوال <span id="current-question">1</span> از <span id="total-questions">10</span>
+            </div> 
+            </div>
             </div>
         </div>
     </div>
-</div>
 
-<script src="/panel/bootstrap-4.6.2-dist/js/bootstrap.min.js"></script>
-<script src="/panel/js/survey.js"></script>
+
+    <script src="/panel/bootstrap-4.6.2-dist/js/bootstrap.min.js"></script>
+    <script src="/panel/js/survey.js"></script>
 
 </body>
+
 </html>
